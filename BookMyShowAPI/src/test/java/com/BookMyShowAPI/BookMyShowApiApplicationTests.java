17a2545cbf0cb9b883611e2ccff60dcd8e473dc2
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-05-24 14:54:13 +0000

    Spring Initializr

---
+++ BookMyShowAPI/src/test/java/com/BookMyShowAPI/BookMyShowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookMyShowAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
